fc8fef20c95b9831699ce60ccd2032e1516943c9
MecheSSI <ssi974@yahoo.fr>
2013-12-29 11:50:39 +0100

    Aération code

--- editeur-de-mission-InterfaceGraphiqueWindowsBuilder/src/view/Editeur.java
+++ editeur-de-mission-InterfaceGraphiqueWindowsBuilder/src/view/Editeur.java
@@ -193,51 +193,70 @@ public class Editeur extends JFrame {
 		//Menu fichier
 		nouveau.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(nouveau);
+		
 		ouvrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(ouvrir);
+		
 		fermer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(fermer);
 		fermer.setEnabled(false);
+		
 		this.fichier.addSeparator();//-------
+		
 		enregistrer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(enregistrer);
 		enregistrer.setEnabled(false);
+		
 		enregistrer_sous.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK));
 		this.fichier.add(enregistrer_sous);
 		enregistrer_sous.setEnabled(false);
+		
 		/*this.fichier.addSeparator();//-------
+		 
 		imprimer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(imprimer);
 		imprimer.setEnabled(false);
 		apercu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(apercu);
 		apercu.setEnabled(false);*/
+		
 		this.fichier.addSeparator();//-------
+		
 		quitter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, KeyEvent.ALT_DOWN_MASK));
 		this.fichier.add(quitter);
-		// Fermeture de la fenêtre en appuyant sur 'Exit'
-		quitter.addActionListener(new QuitListener());
+		quitter.addActionListener(new QuitListener());	// Fermeture de la fenêtre en appuyant sur 'Exit'
+		
 		//Menu edition
 		precedent.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK));
 		this.edition.add(precedent);
+		
 		suivant.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, KeyEvent.CTRL_DOWN_MASK));
 		this.edition.add(suivant);
+		
 		this.edition.addSeparator();//-------
+		
 		couper.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK));
 		this.edition.add(couper);
+		
 		copier.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
 		this.edition.add(copier);
+		
 		coller.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK));
 		this.edition.add(coller);
-		//Menu edition
+		
+		//Menu vue
 		zoom.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, KeyEvent.CTRL_DOWN_MASK));
 		this.vue.add(zoom);
+		
 		dezoom.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, KeyEvent.CTRL_DOWN_MASK));
 		this.vue.add(dezoom);
-		//Menu edition
+		
+		//Menu help
 		aideItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, KeyEvent.CTRL_DOWN_MASK));
 		this.aide.add(aideItem);
+		
 		this.aide.addSeparator();//-------
+		
 		//a_propos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, KeyEvent.CTRL_DOWN_MASK));
 		this.aide.add(a_propos);
 		
