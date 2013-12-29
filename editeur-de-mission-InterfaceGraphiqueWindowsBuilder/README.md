1801c88e952af63dc31e65f8511ecbc6e94a5bde
MecheSSI <ssi974@yahoo.fr>
2013-12-29 12:39:33 +0100

    Ajout Ouvrir Fichier

---
+++ editeur-de-mission-InterfaceGraphiqueWindowsBuilder/README.md
@@ -0,0 +1,2 @@
+editeur-de-mission
+==================
--- editeur-de-mission-InterfaceGraphiqueWindowsBuilder/src/view/Editeur.java
+++ editeur-de-mission-InterfaceGraphiqueWindowsBuilder/src/view/Editeur.java
@@ -4,7 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -196,6 +201,7 @@ public class Editeur extends JFrame {
 		
 		ouvrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(ouvrir);
+		ouvrir.addActionListener(new OpenListener());
 		
 		fermer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, KeyEvent.CTRL_DOWN_MASK));
 		this.fichier.add(fermer);
@@ -278,5 +284,13 @@ public class Editeur extends JFrame {
 			dispose();
 		}
 	} 
+	
+	private class OpenListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser dialogue = new JFileChooser();	 // création de la boîte de dialogue
+            dialogue.showOpenDialog(null);	// affichage
+            //System.out.println("Fichier choisi : " + dialogue.getSelectedFile());	// récupération du fichier sélectionné
+			}
+		}
 
 }
