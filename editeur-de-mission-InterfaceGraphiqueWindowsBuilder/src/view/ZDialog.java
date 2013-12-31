2fcae36991dea5db922d6858653d94b5889d28aa
MecheSSI <ssi974@yahoo.fr>
2013-12-31 13:50:29 +0100

    Modification boite dialogue

--- editeur-de-mission-InterfaceGraphiqueWindowsBuilder/src/view/ZDialog.java
+++ editeur-de-mission-InterfaceGraphiqueWindowsBuilder/src/view/ZDialog.java
@@ -10,23 +10,24 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class ZDialog extends JDialog {
 	  private Fenetre fenetre;
 	  private JButton artefactButton, agentButton;
-	  private JTextField artefact;
-	  private String nomArtefact;
+	  private JTextField urlArtefact, nomArtefact;
 	  private String title;
 	  private boolean modal;
+	  private JLabel nomLabel;
 
   public ZDialog(Fenetre parent, String title, boolean modal){
       super();
       this.fenetre = parent;
       this.title = title;
       this.modal = modal;
-      this.setSize(400, 150);
+      this.setSize(500, 210);
       this.setLocationRelativeTo(null);
       this.setResizable(false);
       this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -40,8 +41,8 @@ public class ZDialog extends JDialog {
 	  JPanel panArtefact = new JPanel();
 	  panArtefact.setBackground(Color.white);
 	  panArtefact.setPreferredSize(new Dimension(350, 60));
-	  artefact = new JTextField();
-	  artefact.setPreferredSize(new Dimension(200, 25));
+	  urlArtefact = new JTextField();
+	  urlArtefact.setPreferredSize(new Dimension(200, 25));
 	  panArtefact.setBorder(BorderFactory.createTitledBorder("Artefact"));
 	  artefactButton = new JButton("URL Artefact");
 	  artefactButton.addActionListener(new ActionListener() {
@@ -50,19 +51,33 @@ public class ZDialog extends JDialog {
 			}
 	    });
 	  panArtefact.add(artefactButton);
-	  panArtefact.add(artefact);
+	  panArtefact.add(urlArtefact);
+	  
+	//Le nom
+	    JPanel panNom = new JPanel();
+	    panNom.setBackground(Color.white);
+	    panNom.setPreferredSize(new Dimension(350, 60));
+	    nomArtefact = new JTextField();
+	    nomArtefact.setPreferredSize(new Dimension(200, 25));
+	    panNom.setBorder(BorderFactory.createTitledBorder("Nom Artefact"));
+	    nomLabel = new JLabel("Saisir un nom :");
+	    panNom.add(nomLabel);
+	    panNom.add(nomArtefact);
 
       JPanel content = new JPanel();
       content.setBackground(Color.white);
       content.add(panArtefact);
+      content.add(panNom);
 
       JPanel control = new JPanel();
       JButton okBouton = new JButton("OK");
     
       okBouton.addActionListener(new ActionListener() {
 		  public void actionPerformed(ActionEvent arg0) {
-			  ajoutObjet();
-			  setVisible(false);
+			  if (nomArtefact.getText().trim().length() != 0 && urlArtefact.getText().trim().length() != 0) {	// Si les deux champs sont remplis
+				  ajoutObjet();
+				  setVisible(false);
+			  }  
 		  }
       }); 
    
@@ -84,13 +99,12 @@ public class ZDialog extends JDialog {
 		JFileChooser chooser = new JFileChooser();
 
 		if (chooser.showOpenDialog(null) == 0) {
-			artefact.setText(chooser.getSelectedFile().toString());
-			nomArtefact = chooser.getName(chooser.getSelectedFile());
+			urlArtefact.setText(chooser.getSelectedFile().toString());
 		}   
   }
   
   public void ajoutObjet() {
-	  this.fenetre.ajouterObjet(nomArtefact, artefact.getText());
+	  this.fenetre.ajouterObjet(nomArtefact.getText(), urlArtefact.getText());
   }
 
 }
